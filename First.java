4324503cb8c6cc49c4df24e884f3e9da1722600b
yejz <yejz>
2016-04-01 11:16:43 +0800

    three

--- First.java
+++ First.java
@@ -1,7 +1,16 @@
 package com.practice.mushi.testproject;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by ontim on 16-4-1.
+ * @author yjz
  */
-public class First {
+public class First extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
